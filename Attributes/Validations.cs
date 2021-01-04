c67dc1ed0ee1d95906e86336a4f60de8c7dc6bba
jim <irawan.jemmy@gmail.com>
2021-01-04 21:38:54 +0800

    more validations and docs

--- Attributes/Validations.cs
+++ Attributes/Validations.cs
@@ -13,4 +13,28 @@ namespace MultiTenancy {
       return ErrorMessage ?? $"{name} can not contain spaces";
     }
   }
+
+  [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+  sealed public class YearRangeAttribute : ValidationAttribute {
+    /// <summary>
+    /// Min offset from the current year
+    /// e.g. when set 100, it means cur year - 100
+    /// </summary>
+    /// <value></value>
+    public int MinOffset { get; set; } = 100;
+    /// <summary>
+    /// Max offset from the current year.
+    /// </summary>    
+    public int MaxOffset { get; set; } = 0;
+
+    public override bool IsValid(object value) {
+      int curYear = DateTime.Now.Year;
+      int y = int.Parse(value?.ToString());
+      return y > (curYear - MinOffset) && y <= (curYear + MaxOffset);
+    }
+
+    public override string FormatErrorMessage(string name) {
+      return ErrorMessage ?? $"{name} is not a valid year";
+    }
+  }
 }
--- Core/IEntity.cs
+++ Core/IEntity.cs
@@ -1,11 +1,20 @@
 using System;
 
 namespace MultiTenancy {
+  /// <summary>
+  /// Whenever an entity implements this interface:
+  /// - it will set the default value to 'now()' for both CreatedAt and UpdatedAt on inserted
+  /// - it will automatically set UpdatedAt = DateTime.Now on updated
+  /// </summary>
   public interface IEntity {
     DateTimeOffset CreatedAt { get; init; }
     DateTimeOffset UpdatedAt { get; set; }
   }
 
+  /// <summary>
+  /// Whenever an entity implements this interface,
+  /// TenantDbContext will set the global filter so that it will only retrieve the entity that belong to the associated TenantId
+  /// </summary>
   public interface ITenantScopedEntity : IEntity {
     string TenantId { get; set; }
   }
