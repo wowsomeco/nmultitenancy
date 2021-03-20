afaacfb7f27cf028d470b7f539c2cc9fb009334f
jim <irawan.jemmy@gmail.com>
2021-03-20 10:32:45 +0800

    feat(Lists): more extensions

--- Extensions/Lists.cs
+++ Extensions/Lists.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace MultiTenancy {
   public static class ListExtensions {
+    public delegate T Reducer<T, U>(T prev, U current);
     public delegate bool Iterator<T>(T item, int idx);
     public delegate void IteratorWithPointer<T>(T item, int idx, bool first, bool last);
 
@@ -29,6 +30,14 @@ namespace MultiTenancy {
       return newList;
     }
 
+    public static T Fold<T, U>(this IEnumerable<U> l, T initialValue, Reducer<T, U> reducer) {
+      T cur = initialValue;
+      foreach (U itm in l) {
+        cur = reducer(cur, itm);
+      }
+      return cur;
+    }
+
     public static List<T> Merge<T>(this List<T> list, params T[] items) {
       if (null != items) {
         foreach (T item in items) {
@@ -47,6 +56,18 @@ namespace MultiTenancy {
       return newList;
     }
 
+    public static T Last<T>(this IList<T> l) {
+      return l.Count > 0 ? l[l.Count - 1] : default(T);
+    }
+
+    public static T RemoveLast<T>(this IList<T> l) where T : class {
+      T t = l.Last();
+      if (null != t) {
+        l.RemoveAt(l.Count - 1);
+      }
+      return t;
+    }
+
     public static List<T> RemoveWhere<T>(this List<T> l, Predicate<T> p) {
       List<T> found = l.FindAll(p);
       if (found.Count > 0) l.RemoveAll(p);
@@ -54,8 +75,37 @@ namespace MultiTenancy {
       return found;
     }
 
+    public static T RemoveSingleWhere<T>(this List<T> l, Predicate<T> p) {
+      T found = l.Find(p);
+      if (found != null) l.Remove(found);
+
+      return found;
+    }
+
     public static bool IsEmpty<T>(this IList<T> l) {
       return null == l || l.Count == 0;
+    }
+
+    public static List<T> RemoveRangeLast<T>(this List<T> l, int count) {
+      List<T> removed = new List<T>();
+      if (l.IsEmpty()) return removed;
+
+      int i = l.Count - 1;
+      while (removed.Count < count) {
+        if (l.IsEmpty()) break;
+
+        removed.Insert(0, l[i]);
+        l.RemoveAt(i);
+        --i;
+      }
+
+      return removed;
+    }
+
+    public static void AddRange<T>(this IList<T> l, T[] colls, int from, int to) {
+      for (int i = from; i < to; ++i) {
+        l.Add(colls[i]);
+      }
     }
 
     public static void SafeAdd<T>(this List<T> l, T item) {
