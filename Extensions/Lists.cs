5b112d6baece3a8372cde01b33adb23e5005091d
jim <irawan.jemmy@gmail.com>
2021-03-22 11:20:31 +0800

    feat(Lists): added more extension

--- Extensions/Lists.cs
+++ Extensions/Lists.cs
@@ -112,5 +112,24 @@ namespace MultiTenancy {
       if (l == null) l = new List<T>();
       l.Add(item);
     }
+
+    public static bool HasDuplicate<T>(this IEnumerable<T> source, out T firstDuplicate) {
+      if (source == null) {
+        throw new ArgumentNullException(nameof(source));
+      }
+
+      var checkBuffer = new HashSet<T>();
+      foreach (var t in source) {
+        if (checkBuffer.Add(t)) {
+          continue;
+        }
+
+        firstDuplicate = t;
+        return true;
+      }
+
+      firstDuplicate = default(T);
+      return false;
+    }
   }
 }
